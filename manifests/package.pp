ab2bbd26b075ff85f7f71a7516716877e3aa9665
Carl Alexander <carlalexander@gmail.com>
2013-04-22 22:39:09 -0400

    Trying to reorder package steps

--- manifests/package.pp
+++ manifests/package.pp
@@ -18,9 +18,15 @@ class logentries::package {
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
   }
 
+  exec { 'add-pgp.mit.edu':
+    command => 'gpg --keyserver pgp.mit.edu --recv-keys C43C79AD && gpg -a --export C43C79AD | apt-key add -',
+    unless  => 'apt-key list | grep -c C43C79AD'
+  }
+
   exec { 'logentries.list':
     command => "echo 'deb http://rep.logentries.com/ quantal main' >> /etc/apt/sources.list.d/logentries.list && apt-get update",
-    creates => '/etc/apt/sources.list.d/logentries.list'
+    creates => '/etc/apt/sources.list.d/logentries.list',
+    require => Exec['add-pgp.mit.edu']
   }
 
   exec { 'add-pgp.mit.edu':
